d8bc303f4f9899fe983c1186a168dac714037616
zenxds <zenxds@gmail.com>
2017-09-12 19:11:22 +0800

    mobx

--- src/container/app.js
+++ src/container/app.js
@@ -1,36 +1,22 @@
 import { Component } from 'react'
 import { observer } from "mobx-react"
-import { observable, action } from "mobx"
 
 import 'normalize.css/normalize.css'
 import './index.less'
 
+import store from './store'
 import Header from './component/header'
 import Content from './component/content'
 import Footer from './component/footer'
 
-import * as actions from '../action'
-
 @observer
 class App extends Component {
-  @observable msg = ''
-
-  componentDidMount() {
-    actions.getHelloMsg().then(data => {
-      this.setMsg(data.helloMsg)
-    })
-  }
-
-  @action
-  setMsg(msg) {
-    this.msg = msg
-  }
-
   render() {
+
     return (
       <div>
         <Header />
-        <Content msg={this.msg} />
+        <Content msg={store.msg} />
         <Footer />
       </div>
     )
