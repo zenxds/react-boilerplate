06ad154c2bf39f75b8f2a6257abd9a1ff47d4c83
zenxds <zenxds@gmail.com>
2017-09-22 19:57:28 +0800

    remove img-loader

--- config/webpack.config.production.js
+++ config/webpack.config.production.js
@@ -67,8 +67,8 @@ module.exports = {
       {
         test: /\.(png|jpe?g|gif|svg)$/,
         use: [
-          'url-loader?limit=8192&name=image/[hash].[ext]',
-          'img-loader'
+          'url-loader?limit=8192&name=image/[hash].[ext]'
+          // 'img-loader'
         ]
       }
     ])
