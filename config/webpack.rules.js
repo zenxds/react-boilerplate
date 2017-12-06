04431cbfa5cb194385aa2b84042d7fcf889fbf0e
zenxds <zenxds@gmail.com>
2017-12-06 21:04:40 +0800

    iconfont

--- config/webpack.rules.js
+++ config/webpack.rules.js
@@ -3,12 +3,7 @@
  */
 module.exports = [
   {
-    test: /\.(woff|woff2|eot|ttf|otf)$/,
+    test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
     use: 'url-loader?limit=10000&name=font/[hash].[ext]'
-  },
-  {
-    test: /\.svg$/,
-    loader: 'svg-sprite-loader',
-    options: {}
   }
 ]
