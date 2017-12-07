7e6a9869f06d657c6e5de7bbe7b6911b696a4773
zenxds <zenxds@gmail.com>
2017-12-07 10:41:07 +0800

    svg image

--- config/webpack.rules.js
+++ config/webpack.rules.js
@@ -3,7 +3,7 @@
  */
 module.exports = [
   {
-    test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
+    test: /\.(woff|woff2|eot|ttf|otf)$/,
     use: 'url-loader?limit=10000&name=font/[hash].[ext]'
   }
 ]
