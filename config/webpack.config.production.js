7aab9c49b57a37c382a02fbc27d91f8ec48a5d1a
zenxds <zenxds@gmail.com>
2018-09-06 12:55:36 +0800

    build css

--- config/webpack.config.production.js
+++ config/webpack.config.production.js
@@ -5,6 +5,8 @@ const webpack = require('webpack')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const WebpackCleanupPlugin = require('webpack-cleanup-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
 
 const rules = require('./webpack.rules')
 module.exports = {
@@ -13,6 +15,22 @@ module.exports = {
   output: {
     path: path.join(__dirname, '../build'),
     filename: 'main.js'
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          output: {
+            ascii_only: true
+          },
+          compress: {
+            drop_console: true
+          }
+        }
+      }),
+      new OptimizeCSSAssetsPlugin({}),
+      new webpack.BannerPlugin(`${moment().format('YYYY-MM-DD HH:mm:ss')}`)
+    ]
   },
   resolve: {
     modules: ['node_modules', 'src']
@@ -131,7 +149,6 @@ module.exports = {
       template: 'template/index.prod.html',
       hash: true,
       random: Math.random().toString().slice(2)
-    }),
-    new webpack.BannerPlugin(`${moment().format('YYYY-MM-DD HH:mm:ss')}`)
+    })
   ]
 }
