9157f6422959e67052c339017060240f1da4e930
zenxds <zenxds@gmail.com>
2019-04-22 19:18:27 +0800

    优化test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,3 +1,9 @@
-test('application', () => {
-  expect(true).toBeTruthy()
+import { mount, shallow } from 'enzyme'
+
+import Header from '../src/component/Header'
+
+test('header', () => {
+  const wrapper = mount(<Header />)
+
+  expect(wrapper.html()).toBeTruthy()
 })
